6616a2659a8bc5cbdcaafd86a14d0050d9c13b50
denmercs <denmercs@gmail.com>
2020-03-23 17:49:46 -0500

    common problem solvign notes

---
+++ problem-solving-patterns/frequency-counter.js
@@ -0,0 +1,54 @@
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+}
+
+console.log(same([1, 2, 3, 2], [9, 4, 4, 1]));
+
+function validAnagram(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+    let letter = arr1[i];
+
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    let letter = arr2[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+console.log("valid anagram results", validAnagram("anagram", "anagram"));
